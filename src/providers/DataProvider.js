6a517b5515abe4189aa78683eee13269000e4f17
Colby Patterson <cpatt@byui.edu>
2022-06-01 15:06:04 -0600

    Added browser router and data provider to index.js

--- src/providers/DataProvider.js
+++ src/providers/DataProvider.js
@@ -3,19 +3,19 @@ import React, { useState } from "react"
 export const DataContext = React.createContext()
 
 const initialSongs = [
-  {id: 1, song: 'Carry On Wayward Song', artist: 'Kansas'}
+  { id: 1, song: 'Carry On Wayward Son', artist: 'Kansas' }
 ]
 
 const DataProvider = (props) => {
   const [songs, setSongs] = useState(initialSongs)
   return (
     <>
-    <DataContext.Provider>
-      value={{
-        songs
-      }}
-      {props.children}
-    </DataContext.Provider>
+      <DataContext.Provider>
+        value={{
+          songs
+        }}
+        {props.children}
+      </DataContext.Provider>
     </>
   )
 }
